eecb00955cd443c03cc640670ae0bf37e35532a0
Israel Brener <israelbrener@gmail.com>
2021-07-13 22:39:44 +0300

    comment 3 in main

--- CouponsProjectIsraelBrener/src/main/java/com/israel/coupons/Main.java
+++ CouponsProjectIsraelBrener/src/main/java/com/israel/coupons/Main.java
@@ -2,7 +2,7 @@ package com.israel.coupons;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//comment
 
 @SpringBootApplication
 public class Main {
